e6600cb413a7d81f6708634a1ff8d1ce7d8224f7
WayneKellman <wayne.n.kellman@gmail.com>
2019-02-24 14:57:16 -0500

    moved to constructor

--- app/src/main/java/wayne/com/javadontliekotlin/utils/DragManageAdapter.kt
+++ app/src/main/java/wayne/com/javadontliekotlin/utils/DragManageAdapter.kt
@@ -5,10 +5,11 @@ import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import wayne.com.javadontliekotlin.ui.startUi.StartViewModel
 
-class DragManageAdapter(adapter: IAdapter, context: Context, dragDirs: Int, swipeDirs: Int, viewModel: StartViewModel) : ItemTouchHelper.SimpleCallback(dragDirs, swipeDirs)
+class DragManageAdapter(
+    private var adapter: IAdapter, context: Context, dragDirs: Int, swipeDirs: Int,
+    private val viewModel: StartViewModel
+) : ItemTouchHelper.SimpleCallback(dragDirs, swipeDirs)
 {
-    var adapter = adapter
-    private val viewModel = viewModel
 
     override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, target: RecyclerView.ViewHolder): Boolean
     {
